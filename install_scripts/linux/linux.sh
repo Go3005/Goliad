27d1b8e1f5d88a993ade5f84557f23a83af7f815
jonathandata1 <jonathanvill00@gmail.com>
2021-08-11 01:32:17 -0500

    Created Install Scripts For MacOS & Linux

---
+++ install_scripts/linux/linux.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+# ---------------------------------------------------------------------------------------------
+#  Goliad OpenVPN Client
+# Linux Install Script
+#  Creation Date: 08/11/2021
+# Author: Jonathan Scott
+
+sudo apt install openvpn peco lynx
